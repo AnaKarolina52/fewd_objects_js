7198e4e9f9f97374944c50a443453a5127357a2b
AnaKarolina52 <karol.teatro@me.com>
2020-02-25 16:01:27 +0000

    commit

--- js/script.js
+++ js/script.js
@@ -17,29 +17,31 @@ var elRooms = document.getElementById('rooms');
 elRooms.textContent = hotel.checkAvailablity();
 
 // Define a constructor function template for hotels
-function Hotel(name, rooms, booked) {
+function Hotel(name, rooms, booked, gym) {
     // Object properties
     this.name = name;
     this.rooms = rooms;
     this.booked = booked;
+    this.gym = gym;
     this.checkAvailablity = function() {
         return this.rooms - this.booked;
     };
 }
 
 // Define to instances of the hotel and their arguments
-var clayHotel = new Hotel('Clayton', 68, 3);
-var lemonHotel = new Hotel('Lemon', 56, 15);
-var ballHotel = new Hotel ('Ball', 50, 23);
+var clayHotel = new Hotel('Clayton', 68, 3, 'Yes');
+var lemonHotel = new Hotel('Lemon', 56, 15, 'Yes');
+var ballHotel = new Hotel ('Ball', 50, 23, 'No');
 
 // Declare a variable to hold the name of the hotel and room availability 
 var details1 = clayHotel.name + ' rooms: ';
-    // add content to an existing variable----
     details1 += clayHotel.checkAvailablity();
-// Get the hotel1 div
 var elHotel1 = document.getElementById('hotel1');
-// Add the variables created onto the empty div
 elHotel1.textContent = details1;
+
+var details1_gym = 'Is there a gym: ' + clayHotel.gym;
+var elHotel1_gym = document.getElementById('hotel1_extra');
+elHotel1_gym.textContent = details1_gym;
 
 var details2 = lemonHotel.name + ' rooms: ';
     details2 += lemonHotel.checkAvailablity();
